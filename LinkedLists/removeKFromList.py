c7f0f83fd57181170aae4df3284dbebec6829cb9
Mounica Kamireddy <kamireddym28@gmail.com>
2018-04-25 23:38:45 -0700

    feat: cf_llists_solution1

--- LinkedLists/removeKFromList.py
+++ LinkedLists/removeKFromList.py
@@ -1,3 +1,14 @@
+'''
+Given a singly linked list of integers l and an integer k, remove all elements from list l that have a value equal to k.
+
+- For l = [3, 1, 2, 3, 4, 5] and k = 3, the output should be
+removeKFromList(l, k) = [1, 2, 4, 5];
+
+- For l = [1, 2, 3, 4, 5, 6, 7] and k = 10, the output should be
+removeKFromList(l, k) = [1, 2, 3, 4, 5, 6, 7].
+'''
+
+
 # Definition for singly-linked list:
 #class ListNode(object):
 #   def __init__(self, x):
