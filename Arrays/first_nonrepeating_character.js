4255ae421ecc17f60ed594fcf1bcac566fd3d5a7
Mounica Kamireddy <kamireddym28@gmail.com>
2018-05-01 23:28:05 -0700

    feat: JavaScript solution

---
+++ Arrays/first_nonrepeating_character.js
@@ -0,0 +1,8 @@
+function firstNotRepeatingCharacter(s) {
+    for (let i=0; i<s.length; i++) {
+        if(s.indexOf(s[i])===s.lastIndexOf(s[i])) {
+            return s[i]
+        }
+    }
+    return '_'
+}
