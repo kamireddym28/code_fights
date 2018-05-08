6e1c466cb5ff9e64c2b96373818597a3100782a9
Mounica Kamireddy <kamireddym28@gmail.com>
2018-05-07 22:46:33 -0700

    feat: Question with examples for reverse integer

--- reverseInteger.py
+++ reverseInteger.py
@@ -1,3 +1,17 @@
+'''
+Given a 32-bit signed integer, reverse digits of an integer.
+
+Eg1: Input: 123
+    Output: 321
+    
+Eg2: Input: -123
+     Output: -321
+     
+Eg3: Input: 120
+     Output: 21
+'''
+
+
 class Solution:
     def reverse(self, x):
         """
