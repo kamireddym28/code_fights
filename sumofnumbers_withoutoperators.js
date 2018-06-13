4f18e156b6a42746aa7781cf5b8ab1e41511494b
Mounica Kamireddy <kamireddym28@gmail.com>
2018-06-12 22:35:41 -0700

    feat: Integers sum w/o using arithmetic operators

--- sumofnumbers_withoutoperators.js
+++ sumofnumbers_withoutoperators.js
@@ -1,8 +1,10 @@
-/**
- * @param {number} a
- * @param {number} b
- * @return {number}
- */
+/*
+Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+
+Example:
+Given a = 1 and b = 2, return 3.
+*/
+
 var getSum = function(a, b) {
     while(true) {
       var sum = (a^b);
