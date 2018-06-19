db080080eec97880c004e48f758abc90fe1c1f3c
Mounica Kamireddy <kamireddym28@gmail.com>
2018-06-18 22:13:15 -0700

    feat: Permutations of a string

---
+++ findPermutations.js
@@ -0,0 +1,25 @@
+function getPermutations(strVar) {
+    // Is there only one character,
+    // then there is only one permutation... return it!
+    if (strVar.length < 2) return strVar;
+
+    // array to hold permutations
+    var permutations = [];
+
+    for (var i=0; i<strVar.length; i++) {
+        var char = strVar[i];
+
+        // No duplicates, skip char if it's used already
+        if (strVar.indexOf(char) != i)
+            continue;
+
+        var remainingString = strVar.slice(0,i) + strVar.slice(i+1,strVar.length);
+
+        for (var subPermutation of getPermutations(remainingString))
+            permutations.push(char + subPermutation)
+
+    }
+    return permutations;
+}
+
+console.log(getPermutations('abc'));
