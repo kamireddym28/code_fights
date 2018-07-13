38a1a4d411ee15642a17891f79529c04229eb8ac
Mounica Kamireddy <kamireddym28@gmail.com>
2018-07-12 22:49:11 -0700

    feat: Transpose matrix question with examples

---
+++ transpose_matrix.py
@@ -0,0 +1,11 @@
+'''
+Given a matrix A, return the transpose of A.
+
+The transpose of a matrix is the matrix flipped over it's main diagonal, switching the row and column indices of the matrix.
+
+Eg1: Input: [[1,2,3],[4,5,6],[7,8,9]]
+     Output: [[1,4,7],[2,5,8],[3,6,9]]
+     
+Eg2: Input: [[1,2,3],[4,5,6]]
+     Output: [[1,4],[2,5],[3,6]]
+'''
